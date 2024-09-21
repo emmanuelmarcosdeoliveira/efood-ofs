b8148a179ca95f22188adeb12d8a9273fef84956
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-09-21 16:57:18 -0300

    chore: a new route has been inserted for the Restaurant page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Restaurant from './pages/Restaurant';
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/restaurante" element={<Restaurant />} />
     </Routes>
   );
 };
